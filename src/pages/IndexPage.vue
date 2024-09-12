876ff513fb41edd2d22cd4c2333a163f3e5894ed
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-09-12 19:06:58 +0900

    글 가져오기

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -20,6 +20,7 @@
             fugit, sed quia consequuntur magni dolores eos qui ratione
             voluptatem.
           </span>
+          <br />
           <span class="Learn-more"> Learn more </span>
         </div>
       </div>
@@ -31,6 +32,30 @@
         srcset="img/rectangle@2x.png 2x, img/rectangle@3x.png 3x"
         class="Rectangle"
       />
+      <span class="Sed-ut-perspiciatis-unde-omnis">
+        Sed ut perspiciatis unde omnis
+      </span>
+      <span
+        class="There-are-many-variations-of-passages-of-Lorem-Ipsum-available-but-the-majority-have-suffered-alter"
+      >
+        There are many variations of passages of Lorem Ipsum available, but the
+        majority have suffered alteration in some form, by injected humour, or
+        randomised words which don't look even slightly believable. If you are
+        going to use a passage of Lorem Ipsum, you need to be sure there isn't
+        anything embarrassing hidden in the middle of text. All the Lorem Ipsum
+        generators on the Internet tend to repeat predefined chunks as
+        necessary.
+      </span>
+      <span
+        class="Sed-ut-perspiciatis-unde-omnis-iste-natus-error-sit-voluptatem-accusantium-doloremque-laudantium-to"
+      >
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore.
+      </span>
+      <span class="Subscribe-to-our-newsletter">
+        Subscribe to our newsletter
+      </span>
     </div>
 
     <!-- <q-page class="row items-center justify-evenly">

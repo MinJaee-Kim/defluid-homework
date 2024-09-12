d0e62f8528ebe705bcc13af025e63c00c4ef2fbf
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-09-12 18:18:36 +0900

    이미지추가, css요소 가져옴

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,33 @@
 <template>
-  <div class="Rectangle-3">
-    <span class="Snap-photos-and-share-like-never-before">
-      Snap photos and share like never before
-    </span>
+  <div class="Screen">
+    <div class="Rectangle-3">
+      <span class="Snap-photos-and-share-like-never-before">
+        Snap photos and share like never before
+      </span>
 
-    <div class="Group">
-      <img
-        src="img/ellipse.png"
-        srcset="img/ellipse@2x.png 2x, img/ellipse@3x.png 3x"
-        class="Ellipse"
-      />
-      <span class="Sed-ut-perspiciatis"> Sed ut perspiciatis </span>
+      <div class="Section-1">
+        <div class="Group">
+          <img
+            src="../img/ellipse.png"
+            srcset="../img/ellipse@2x.png 2x, ../img/ellipse@3x.png 3x"
+            class="Ellipse"
+          />
+          <span class="Sed-ut-perspiciatis"> Sed ut perspiciatis </span>
+          <span
+            class="Nemo-enim-ipsam-voluptatem-quia-voluptas-sit-aspernatur-aut-odit-aut-fugit-sed-quia-consequuntur-ma"
+          >
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit, sed quia consequuntur magni dolores eos qui ratione
+            voluptatem.
+          </span>
+          <span class="Learn-more"> Learn more </span>
+        </div>
+      </div>
     </div>
 
-    <q-page class="row items-center justify-evenly">
+    <!-- <q-page class="row items-center justify-evenly">
       <card-component lorem=""></card-component>
-    </q-page>
+    </q-page> -->
   </div>
 </template>
 
@@ -23,7 +35,7 @@
 // import { ref } from 'vue';
 // import { Todo, Meta } from 'components/models';
 // import ExampleComponent from 'components/ExampleComponent.vue';
-import CardComponent from 'components/CardComponent.vue';
+// import CardComponent from 'components/CardComponent.vue';
 
 defineOptions({
   name: 'IndexPage'

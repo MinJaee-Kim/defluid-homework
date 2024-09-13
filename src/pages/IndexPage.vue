40a244a9af07f9921f3198686f805f2bdc695501
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-09-13 19:30:59 +0900

    css수정 등

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,71 @@
 <template>
-  <div class="Screen"></div>
+  <div class="Screen">
+    <div class="Rectangle-3">
+      <span class="Word-1"> Snap photos and share like never before </span>
+    </div>
+    <div class="Section-1">
+      <div class="Group">
+        <div class="Ellipse">
+          <img
+            src="../img/ellipse.png"
+            srcset="img/ellipse@2x.png 2x, img/ellipse@3x.png 3x"
+            class="Ellipse"
+          />
+        </div>
+        <span class="User-Title"> Sed ut perspiciatis </span>
+        <div>
+          <span class="User-Word">
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit, sed quia consequuntur magni dolores eos qui ratione
+            voluptatem.
+          </span>
+        </div>
+        <div>
+          <span class="Learn-more"> Learn more </span>
+        </div>
+      </div>
+      <div class="Group">
+        <div class="Ellipse">
+          <img
+            src="../img/ellipse.png"
+            srcset="img/ellipse@2x.png 2x, img/ellipse@3x.png 3x"
+            class="Ellipse"
+          />
+        </div>
+        <span class="User-Title"> Sed ut perspiciatis </span>
+        <div>
+          <span class="User-Word">
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit, sed quia consequuntur magni dolores eos qui ratione
+            voluptatem.
+          </span>
+        </div>
+        <div>
+          <span class="Learn-more"> Learn more </span>
+        </div>
+      </div>
+      <div class="Group">
+        <div class="Ellipse">
+          <img
+            src="../img/ellipse.png"
+            srcset="img/ellipse@2x.png 2x, img/ellipse@3x.png 3x"
+            class="Ellipse"
+          />
+        </div>
+        <span class="User-Title"> Sed ut perspiciatis </span>
+        <div>
+          <span class="User-Word">
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit, sed quia consequuntur magni dolores eos qui ratione
+            voluptatem.
+          </span>
+        </div>
+        <div>
+          <span class="Learn-more"> Learn more </span>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <!-- <q-page class="row items-center justify-evenly">
       <card-component lorem=""></card-component>

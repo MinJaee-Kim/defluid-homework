c1a6dec9441400b11a0d3287ad7900342b2b6355
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-09-13 20:01:47 +0900

    이미지 텍스트박스

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,6 +3,7 @@
     <div class="Rectangle-3">
       <span class="Word-1"> Snap photos and share like never before </span>
     </div>
+    <!-- 머리 -->
     <div class="Section-1">
       <div class="Group">
         <div class="Ellipse">
@@ -28,7 +29,7 @@
         <div class="Ellipse">
           <img
             src="../img/ellipse.png"
-            srcset="img/ellipse@2x.png 2x, img/ellipse@3x.png 3x"
+            srcset="../img/ellipse@2x.png 2x, ../img/ellipse@3x.png 3x"
             class="Ellipse"
           />
         </div>
@@ -48,7 +49,7 @@
         <div class="Ellipse">
           <img
             src="../img/ellipse.png"
-            srcset="img/ellipse@2x.png 2x, img/ellipse@3x.png 3x"
+            srcset="../img/ellipse@2x.png 2x, ../img/ellipse@3x.png 3x"
             class="Ellipse"
           />
         </div>
@@ -65,6 +66,37 @@
         </div>
       </div>
     </div>
+    <!-- 유저 -->
+    <div class="Image-Box">
+      <div class="Box-Word1">Sed ut perspiciatis unde omnis</div>
+      <div class="Box-Word2">
+        There are many variations of passages of Lorem Ipsum available, but the
+        majority have suffered alteration in some form, by injected humour, or
+        randomised words which don't look even slightly believable. If you are
+        going to use a passage of Lorem Ipsum, you need to be sure there isn't
+        anything embarrassing hidden in the middle of text. All the Lorem Ipsum
+        generators on the Internet tend to repeat predefined chunks as
+        necessary.
+      </div>
+      <div class="contour"></div>
+      <div class="Box-Word2">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore.
+      </div>
+      <div class="Box-Word4">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore.
+      </div>
+      <div class="Box-Word5">Subscribe to our newsletter</div>
+      <div class="Text-Box"></div>
+    </div>
+    <!-- <img
+      src="../img/rectangle.png"
+      srcset="../img/rectangle@2x.png 2x, ../img/rectangle@3x.png 3x"
+      class="Rectangle"
+    /> -->
   </div>
 
   <!-- <q-page class="row items-center justify-evenly">

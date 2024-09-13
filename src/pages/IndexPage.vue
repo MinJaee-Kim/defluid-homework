feb40292508182095196eab92b489115be5004c1
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-09-13 16:56:38 +0900

    다 날리고 처음부터 ㄱ

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,67 +1,9 @@
 <template>
-  <div class="Screen">
-    <div class="Rectangle-3">
-      <span class="Snap-photos-and-share-like-never-before">
-        Snap photos and share like never before
-      </span>
+  <div class="Screen"></div>
 
-      <div class="Section-1">
-        <div class="Group">
-          <img
-            src="../img/ellipse.png"
-            srcset="../img/ellipse@2x.png 2x, ../img/ellipse@3x.png 3x"
-            class="Ellipse"
-          />
-          <span class="Sed-ut-perspiciatis"> Sed ut perspiciatis <br /></span>
-          <span
-            class="Nemo-enim-ipsam-voluptatem-quia-voluptas-sit-aspernatur-aut-odit-aut-fugit-sed-quia-consequuntur-ma"
-          >
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-            fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem.
-          </span>
-          <br />
-          <span class="Learn-more"> Learn more </span>
-        </div>
-      </div>
-    </div>
-
-    <div class="Section-2">
-      <img
-        src="../img/rectangle.png"
-        srcset="img/rectangle@2x.png 2x, img/rectangle@3x.png 3x"
-        class="Rectangle"
-      />
-      <span class="Sed-ut-perspiciatis-unde-omnis">
-        Sed ut perspiciatis unde omnis
-      </span>
-      <span
-        class="There-are-many-variations-of-passages-of-Lorem-Ipsum-available-but-the-majority-have-suffered-alter"
-      >
-        There are many variations of passages of Lorem Ipsum available, but the
-        majority have suffered alteration in some form, by injected humour, or
-        randomised words which don't look even slightly believable. If you are
-        going to use a passage of Lorem Ipsum, you need to be sure there isn't
-        anything embarrassing hidden in the middle of text. All the Lorem Ipsum
-        generators on the Internet tend to repeat predefined chunks as
-        necessary.
-      </span>
-      <span
-        class="Sed-ut-perspiciatis-unde-omnis-iste-natus-error-sit-voluptatem-accusantium-doloremque-laudantium-to"
-      >
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore.
-      </span>
-      <span class="Subscribe-to-our-newsletter">
-        Subscribe to our newsletter
-      </span>
-    </div>
-
-    <!-- <q-page class="row items-center justify-evenly">
+  <!-- <q-page class="row items-center justify-evenly">
       <card-component lorem=""></card-component>
     </q-page> -->
-  </div>
 </template>
 
 <script setup lang="ts">

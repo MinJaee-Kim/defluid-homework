adc575bca1d81f83026fadd94194a159dc6fe3dd
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-09-19 16:20:32 +0900

    년도 정렬 기능 완료

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -47,6 +47,7 @@ export default {
         srcset: 'src/img/image-italy@2x.png 2x, src/img/image-italy@3x.png 3x',
         title: 'Italy, Pica',
         year: 1173,
+        location: 'Europe',
         detail:
           "The Leaning Tower of Pisa, or simply the Tower of Pisa (torre di Pisa), is the campanile, or freestanding bell tower, of Pisa Cathedral. It is known for its nearly four-degree lean, the result of an unstable foundation. The tower is one of three structures in the Pisa's Cathedral Square (Piazza del Duomo), which includes the cathedral and Pisa Baptistry."
       },
@@ -56,6 +57,7 @@ export default {
         srcset: 'src/img/image-spain@2x.png 2x, src/img/image-spain@3x.png 3x',
         title: 'Spain, Sagrada Família',
         year: 1882,
+        location: 'Europe',
         detail:
           'The Basílica i Temple Expiatori de la Sagrada Família, otherwise known as Sagrada Família, is a church under construction in the Eixample district of Barcelona, Catalonia, Spain. It is the largest unfinished Catholic church in the world. Designed by Catalan architect Antoni Gaudí (1852–1926), in 2005 his work on Sagrada Família was added to an existing (1984) UNESCO World Heritage Site, "Works of Antoni Gaudí". On 7 November 2010, Pope Benedict XVI consecrated the church and proclaimed it a minor basilica.'
       },
@@ -65,6 +67,7 @@ export default {
         srcset: 'src/img/image-us@2x.png 2x, src/img/image-us@3x.png 3x',
         title: 'US, Fallingwater',
         year: 1935,
+        location: 'America',
         detail:
           "Fallingwater is a house designed by the architect Frank Lloyd Wright in 1935. Situated in the Mill Run section of Stewart township, in the Laurel Highlands of southwest Pennsylvania, about 70 miles (110 km) southeast of Pittsburgh in the United States, it is built partly over a waterfall on the Bear Run river. The house was designed to serve as a weekend retreat for Liliane and Edgar J. Kaufmann, the owner of Pittsburgh's Kaufmann's Department Store."
       },
@@ -75,11 +78,26 @@ export default {
           'src/img/image-russia@2x.png 2x, src/img/image-russia@3x.png 3x',
         title: "Russia, Saint Basil's Cathedral",
         year: 1561,
+        location: 'Europe',
         detail:
           "The Cathedral of Vasily the Blessed (Russian: Собор Василия Блаженного, romanized: Sobor Vasiliya Blazhennogo), known in English as Saint Basil's Cathedral, is an Orthodox church in Red Square of Moscow, and is one of the most popular cultural symbols of Russia."
       }
     ]);
+    const FilterPlaceDetails = ref([
+      {
+        index: 1,
+        image: '',
+        srcset: '',
+        title: '',
+        year: 1,
+        location: '',
+        detail: ''
+      }
+    ]);
     const PhotoData = ref();
+    const saveCardData = ref('');
+
+    FilterPlaceDetails.value = PlaceDetails.value;
 
     const mixing = () => {
       for (let i = 0; i < UserDetail.value.length; i++) {
@@ -88,6 +106,24 @@ export default {
         UserDetail.value[0] = UserDetail.value[RandomNumber];
         UserDetail.value[RandomNumber] = store;
       }
+    };
+
+    const alignCard = (card: string) => {
+      saveCardData.value = card;
+      if (card == 'All') {
+        FilterPlaceDetails.value = PlaceDetails.value.filter((value) => {
+          return year.value < value.year;
+        });
+      } else {
+        FilterPlaceDetails.value = PlaceDetails.value.filter((value) => {
+          return value.location == card && year.value < value.year;
+        });
+      }
+    };
+
+    const setYearAndAlignCard = (num: number) => {
+      year.value = num;
+      alignCard(saveCardData.value);
     };
 
     const setPhoto = async () => {
@@ -119,10 +155,13 @@ export default {
       UserDetail,
       PhotoData,
       PlaceDetails,
+      FilterPlaceDetails,
       location,
       locationOption,
       email,
-      year
+      year,
+      alignCard,
+      setYearAndAlignCard
     };
   }
 };
@@ -200,11 +239,17 @@ export default {
             flat
             rounded
             :options="locationOption"
+            @update:model-value="alignCard"
           />
         </div>
         <div class="Frame-2">
           <div class="Number-Align">
-            <q-btn round color="black" label="1000" @click="year = 1000" />
+            <q-btn
+              round
+              color="black"
+              label="1000"
+              @click="setYearAndAlignCard(1000)"
+            />
           </div>
           <div
             :class="{ 'Non-Select-Countour': year < 1300 }"
@@ -215,7 +260,7 @@ export default {
               round
               color="black"
               label="1300"
-              @click="year = 1300"
+              @click="setYearAndAlignCard(1300)"
               :class="{ 'Non-Select-Round': year < 1300 }"
             />
           </div>
@@ -228,7 +273,7 @@ export default {
               round
               color="black"
               label="1700"
-              @click="year = 1700"
+              @click="setYearAndAlignCard(1700)"
               :class="{ 'Non-Select-Round': year < 1700 }"
             />
           </div>
@@ -241,7 +286,7 @@ export default {
               round
               color="black"
               label="2000"
-              @click="year = 2000"
+              @click="setYearAndAlignCard(2000)"
               :class="{ 'Non-Select-Round': year < 2000 }"
             />
           </div>
@@ -251,7 +296,7 @@ export default {
     <!-- 정렬 박스 -->
     <q-scroll-area class="Location-Area" id="scroll-area-with-virtual-scroll-1">
       <q-virtual-scroll
-        :items="PlaceDetails"
+        :items="FilterPlaceDetails"
         virtual-scroll-horizontal
         v-slot="{ item }"
       >
